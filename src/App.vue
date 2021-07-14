d734226fa8af3d69f45997a816079bd3a8c999bc
Laurence J Marcano <66771560+laurex94@users.noreply.github.com>
2021-07-14 09:11:57 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -48,17 +48,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss">
-.fade-enter-active,
-.fade-leave-active {
-  transition-duration: 0.3s;
-  transition-property: opacity;
-  transition-timing-function: ease;
-}
-
-.fade-enter,
-.fade-leave-active {
-  opacity: 0
-}
-</style>
